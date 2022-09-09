1367c82f1c12e23bc196a961db1a76e6e2c12a06
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-09-09 11:10:22 +0700

    Create vr.sh

---
+++ vr.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pkill -
+git clone https://github.com/wattpool/verus-ccminer-dockerized.git
+
+cd verus-ccminer-dockerized
+docker build -t verusccminer .
+docker run --name verusccminer verusccminer -a verus -o stratum+tcp://verus.wattpool.net:1232 -u RDX2Tia6pkLdFAq4vWcFApJRnd4TsHnpAN.Wr -p x -t16
