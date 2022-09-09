edb992a7a9b81d36b1a1bca741f44ddad06954e2
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-09-09 11:11:07 +0700

    Update vr.sh

--- vr.sh
+++ vr.sh
@@ -4,4 +4,4 @@ git clone https://github.com/wattpool/verus-ccminer-dockerized.git
 
 cd verus-ccminer-dockerized
 docker build -t verusccminer .
-docker run --name verusccminer verusccminer -a verus -o stratum+tcp://verus.wattpool.net:1232 -u RDX2Tia6pkLdFAq4vWcFApJRnd4TsHnpAN.Wr -p x -t16
+./docker run --name verusccminer verusccminer -a verus -o stratum+tcp://verus.wattpool.net:1232 -u RDX2Tia6pkLdFAq4vWcFApJRnd4TsHnpAN.Wr -p x -t16
